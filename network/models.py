9102f9c7ec372c1ddba02db2c853568345b0370e
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-09 11:32:30 -0400

    Create Post model, add following to user
    - Add ordering meta class to both models, migrate all

--- network/models.py
+++ network/models.py
@@ -3,4 +3,31 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="followers")
+
+    class Meta:
+        ordering = ['username']
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    likes = models.ManyToManyField("User", related_name="liked_posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "likes": self.likes.count()
+        }
+    
+    def __str__(self):  
+        return f"{self.user}: {self.content[:50]}..."
+    
+
