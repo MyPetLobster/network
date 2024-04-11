9e2505096dee6e1c61b631787a0a204441991a8b
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-11 08:06:44 -0400

    Move like, heart JS to scripts.js

--- network/static/network/scripts.js
+++ network/static/network/scripts.js
@@ -1,26 +1,52 @@
-const editPostLinks = document.querySelectorAll('.edit-post-link');
-editPostLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        const editPostForm = link.parentElement.parentElement.previousElementSibling;
-        editPostForm.classList.toggle('hidden');
-        editPostForm.querySelector('.cancel-edit').addEventListener('click', () => {
-            editPostForm.classList.add('hidden');
-        });
 
-        editPostContent = editPostForm.querySelector('.edit-post-content');
-        editPostContent.focus();
-        editPostForm.onsubmit = (event) => {
-            event.preventDefault(); 
-            // Retrieve the CSRF token from Django hidden input field
+document.addEventListener('DOMContentLoaded', () => {
+    const editPostLinks = document.querySelectorAll('.edit-post-link');
+    editPostLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            const editPostForm = link.parentElement.parentElement.previousElementSibling;
+            editPostForm.classList.toggle('hidden');
+            editPostForm.querySelector('.cancel-edit').addEventListener('click', () => {
+                editPostForm.classList.add('hidden');
+            });
+
+            editPostContent = editPostForm.querySelector('.edit-post-content');
+            editPostContent.focus();
+            editPostForm.onsubmit = (event) => {
+                event.preventDefault(); 
+                // Retrieve the CSRF token from Django hidden input field
+                const csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+                const formData = new FormData();
+                formData.append('content', editPostContent.value);
+                console.log('Submitting edit post form...');
+                fetch(`edit_post/${editPostForm.querySelector('.post-id').textContent}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        content: editPostContent.value
+                    }),
+                    headers: {
+                        'X-CSRFToken': csrf_token,
+                    }
+                })
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result);
+                    const postId = editPostForm.querySelector('.post-id').textContent;
+                    const postContentElement = document.querySelector(`#post-content-${postId}`);
+                    postContentElement.textContent = result.content;
+                    editPostForm.classList.add('hidden');
+                });
+            };
+        });
+    });
+
+
+    heartIcons = document.querySelectorAll('.heart-icon');
+    heartIcons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const postId = icon.id.split('-')[2];
             const csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-            const formData = new FormData();
-            formData.append('content', editPostContent.value);
-            console.log('Submitting edit post form...');
-            fetch(`edit_post/${editPostForm.querySelector('.post-id').textContent}`, {
+            fetch(`like_post/${postId}`, {
                 method: 'PUT',
-                body: JSON.stringify({
-                    content: editPostContent.value
-                }),
                 headers: {
                     'X-CSRFToken': csrf_token,
                 }
@@ -28,11 +54,34 @@ editPostLinks.forEach(link => {
             .then(response => response.json())
             .then(result => {
                 console.log(result);
-                const postId = editPostForm.querySelector('.post-id').textContent;
-                const postContentElement = document.querySelector(`#post-content-${postId}`);
-                postContentElement.textContent = result.content;
-                editPostForm.classList.add('hidden');
+                const likesDiv = icon.parentElement;
+                const likesCount = likesDiv.querySelector('.card-text');
+                likesCount.textContent = `${result.likes}`;
+                const textSpan = document.createElement('span');
+                textSpan.textContent = result.likes === 1 ? ' person/bot liked this post' : ' people/bots liked this post';
+                textSpan.classList.add('small-grey-text');
+                likesCount.appendChild(textSpan);
+                if (result.liked) {
+                    console.log('liked');
+                    const emptyHeart = document.querySelector(`#empty-heart-path-${postId}`)
+                    const redHeart = document.querySelector(`#red-heart-path-${postId}`)
+                    if (emptyHeart) {
+                        emptyHeart.style.fill = '#F94056';
+                    } else {
+                        redHeart.style.fill = '#F94056';
+                    }
+                } else {
+                    console.log('unliked');
+                    const redHeart = document.querySelector(`#red-heart-path-${postId}`)
+                    const emptyHeart = document.querySelector(`#empty-heart-path-${postId}`)
+                    if (redHeart) {
+                        redHeart.style.fill = '#ffffff';
+                    } else {
+                        emptyHeart.style.fill = '#ffffff';
+                    }
+                    
+                }
             });
-        };
+        });
     });
 });
