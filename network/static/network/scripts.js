0d03964b7e8c8df4359f8d2d0afaeddc8e81fe8e
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-12 10:16:19 -0400

    Remove timeout topnav, etc
    Exclude arrow dynamic sizing card post user

--- network/static/network/scripts.js
+++ network/static/network/scripts.js
@@ -179,20 +179,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const collapseTopPagination = document.getElementById('collapse-top-pagination');
 
     collapseTopPagination.addEventListener('mouseover', () => {
-        topPagination.style.transitionDuration = '5s';
-        topPagination.style.transform = 'scale(0.01)';
-        // start timer to collapse topPagination
-        setTimeout(() => {
-            sessionStorage.setItem('topPagination', 'collapsed')
-            expandTopPagination.classList.remove('hidden');
-            topPagination.classList.add('hidden');
-            collapseTopPagination.classList.add('hidden');
-        }, 5000);
-        collapseTopPagination.addEventListener('mouseout', () => {
-            topPagination.style.transform = 'scale(1)';
-            // stop timer
-            clearTimeout();
-        });
+        topPagination.style.transitionDuration = '3s';
+        topPagination.style.transform = 'scale(0.25)';
+    });
+    collapseTopPagination.addEventListener('mouseout', () => {
+        topPagination.style.transform = 'scale(1)';
     });
 
     expandTopPagination.addEventListener('mouseover', () => {
