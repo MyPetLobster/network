209105d25b9b0ea6719a2378482ab5aa3dce669c
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-13 12:56:07 -0400

    Reorder urlpatterns in urls.py

--- network/urls.py
+++ network/urls.py
@@ -4,19 +4,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.landing, name="landing"),
     path("all_posts", views.index, name="all_posts"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("", views.landing, name="landing"),
     path("create_post", views.create_post, name="create_post"),
     path("reply_post/<int:post_id>", views.reply_post, name="reply_post"),
     path("post/reply_post/<int:post_id>", views.reply_post, name="reply_post"),
+    path("edit_post/<int:post_id>", views.edit_post, name="edit_post"),
+    path("profile/edit_post/<int:post_id>", views.edit_post, name="edit_post"),
     path("profile/<int:user_id>", views.profile, name="profile"),
     path("edit_profile/<int:user_id>", views.edit_profile, name="edit_profile"),
     path("following", views.following, name="following"),
-    path("edit_post/<int:post_id>", views.edit_post, name="edit_post"),
-    path("profile/edit_post/<int:post_id>", views.edit_post, name="edit_post"),
     path("like_post/<int:post_id>", views.like_post, name="like_post"),
     path("post/like_post/<int:post_id>", views.like_post, name="like_post"),
     path("profile/like_post/<int:post_id>", views.like_post, name="like_post"),
