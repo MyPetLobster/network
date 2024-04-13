635e626e26e8b3fbc9f533a66c93efc861359cb7
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-04-13 13:06:42 -0400

    Add docstring describing custom_filter

--- network/templatetags/custom_filters.py
+++ network/templatetags/custom_filters.py
@@ -5,6 +5,11 @@ register = template.Library()
 # get pagination range
 @register.filter
 def get_range(current_page, num_pages):
+    '''
+    Returns a range of page numbers to display in the pagination bar.
+    Used to generate .page-item elements in the pagination bar.
+    '''
+    
     if current_page <= 5:
         return range(1, min(10, num_pages) + 1)
     elif current_page >= num_pages - 5:
